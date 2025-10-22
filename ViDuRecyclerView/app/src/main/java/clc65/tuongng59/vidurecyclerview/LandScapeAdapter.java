ef536841f05ba7bf344a81f1f954a1425c7495d9
TuongNG59 <nhuynhtuong59@gmail.com>
2025-10-22 12:42:19 +0700

    First Commit

---
+++ ViDuRecyclerView/app/src/main/java/clc65/tuongng59/vidurecyclerview/LandScapeAdapter.java
@@ -0,0 +1,49 @@
+package clc65.tuongng59.vidurecyclerview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder{
+        TextView tvCaption;
+        ImageView ivLandScape;
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCation);
+            ivLandScape = itemView.findViewById(R.id.imageViewLand);
+        }
+    }
+}
