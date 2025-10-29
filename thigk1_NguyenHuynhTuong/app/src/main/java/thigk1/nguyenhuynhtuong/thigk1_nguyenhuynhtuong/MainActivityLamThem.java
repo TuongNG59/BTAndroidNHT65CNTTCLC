fdcecba653f4c7e579e7efd063165c2cbf35b1cb
TuongNG59 <nhuynhtuong59@gmail.com>
2025-10-29 08:52:49 +0700

    AcitivityLamThem

--- thigk1_NguyenHuynhTuong/app/src/main/java/thigk1/nguyenhuynhtuong/thigk1_nguyenhuynhtuong/MainActivityLamThem.java
+++ thigk1_NguyenHuynhTuong/app/src/main/java/thigk1/nguyenhuynhtuong/thigk1_nguyenhuynhtuong/MainActivityLamThem.java
@@ -1,6 +1,11 @@
 package thigk1.nguyenhuynhtuong.thigk1_nguyenhuynhtuong;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,83 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityLamThem extends AppCompatActivity {
+    private TextView tvQuestion, tvScore;
+    private RadioGroup rgOptions;
+    private RadioButton rb1, rb2, rb3, rb4;
+    private Button btnNext;
+
+    private String[] questions = {
+            "Android được phát triển dựa trên ngôn ngữ nào?",
+            "File giao diện trong Android thường có đuôi là gì?",
+            "Thành phần nào dùng để hiển thị danh sách?",
+            "Tên công cụ dùng để biên dịch project Android là?"
+    };
+
+    private String[][] options = {
+            {"Python", "Java", "C++", "Swift"},
+            {"html", "xml", "json", "css"},
+            {"ListView", "ImageView", "TextView", "EditText"},
+            {"Gradle", "Maven", "Android Studio", "Logcat"}
+    };
+
+    private int[] correctAnswers = {1, 1, 0, 0};
+    private int index = 0;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_lam_them);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tvQuestion = findViewById(R.id.tvQuestion);
+        tvScore = findViewById(R.id.tvScore);
+        rgOptions = findViewById(R.id.rgOptions);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
+        rb4 = findViewById(R.id.rb4);
+        btnNext = findViewById(R.id.btnNext);
+
+        loadQuestion();
+
+        btnNext.setOnClickListener(v -> {
+            int selectedId = rgOptions.getCheckedRadioButtonId();
+            if (selectedId == -1) {
+                Toast.makeText(this, "Vui lòng chọn 1 đáp án!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int answerIndex = rgOptions.indexOfChild(findViewById(selectedId));
+            if (answerIndex == correctAnswers[index]) {
+                score++;
+                Toast.makeText(this, "Chính xác!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Sai rồi!", Toast.LENGTH_SHORT).show();
+            }
+
+            index++;
+            if (index < questions.length) {
+                loadQuestion();
+            } else {
+                tvQuestion.setText("KẾT THÚC!");
+                tvScore.setText("Điểm của bạn là: " + score + "/" + questions.length);
+                btnNext.setEnabled(false);
+                rgOptions.clearCheck();
+                rb1.setEnabled(false);
+                rb2.setEnabled(false);
+                rb3.setEnabled(false);
+                rb4.setEnabled(false);
+            }
         });
+    }
+
+    private void loadQuestion() {
+        tvQuestion.setText(questions[index]);
+        rb1.setText(options[index][0]);
+        rb2.setText(options[index][1]);
+        rb3.setText(options[index][2]);
+        rb4.setText(options[index][3]);
+        tvScore.setText("Câu " + (index + 1) + "/" + questions.length);
+        rgOptions.clearCheck();
     }
 }
