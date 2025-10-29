5bb2a740c75dc8aaf0b6709476b5cfb584092315
TuongNG59 <nhuynhtuong59@gmail.com>
2025-10-29 09:01:54 +0700

    Activity_item4

--- thigk1_NguyenHuynhTuong/app/src/main/java/thigk1/nguyenhuynhtuong/thigk1_nguyenhuynhtuong/CTdetailActivity.java
+++ thigk1_NguyenHuynhTuong/app/src/main/java/thigk1/nguyenhuynhtuong/thigk1_nguyenhuynhtuong/CTdetailActivity.java
@@ -1,4 +1,32 @@
 package thigk1.nguyenhuynhtuong.thigk1_nguyenhuynhtuong;
 
-public class CTdetailActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class CTdetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item4);
+
+        ImageView img = findViewById(R.id.imgDetail);
+        TextView tvTitle = findViewById(R.id.tvTitleDetail);
+        TextView tvDesc = findViewById(R.id.tvDescDetail);
+        MaterialButton btnBack = findViewById(R.id.btnBack);
+
+        int imageRes = getIntent().getIntExtra("image", 0);
+        String title = getIntent().getStringExtra("title");
+        String desc = getIntent().getStringExtra("desc");
+
+        img.setImageResource(imageRes);
+        tvTitle.setText(title);
+        tvDesc.setText(desc);
+
+        btnBack.setOnClickListener(v -> finish());
+    }
 }
