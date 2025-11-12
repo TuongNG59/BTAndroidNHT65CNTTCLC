8a461c52dd14d8a5b1bf760de5ae6cc4760c34d8
TuongNG59 <nhuynhtuong59@gmail.com>
2025-11-12 16:20:48 +0700

    update menu + main activity

--- TopAppBarEx/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/topappbarex/MainActivity.java
+++ TopAppBarEx/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/topappbarex/MainActivity.java
@@ -1,6 +1,7 @@
 package clc65.tuongng59.nguyenhuynhtuong.topappbarex;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
 
+public class MainActivity extends AppCompatActivity {
+    private MaterialToolbar topAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        topAppBar = findViewById(R.id.topAppBar);
+        // Navigation icon (hamburger/menu)
+        topAppBar.setNavigationOnClickListener(v ->
+                Toast.makeText(this, "Mở menu điều hướng", Toast.LENGTH_SHORT).show()
+        );
+        // Action menu item click
+        topAppBar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_search:
+                    Toast.makeText(this, "Tìm kiếm...", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_settings:
+                    Toast.makeText(this, "Mở cài đặt", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
         });
     }
 }
