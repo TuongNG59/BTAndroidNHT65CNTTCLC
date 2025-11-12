b7c45d8077d1798f472fafff33416dcb9a161494
TuongNG59 <nhuynhtuong59@gmail.com>
2025-11-12 16:02:57 +0700

    Complete BottomNavigationEx

--- BottomNavigationViewEx/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/bottomnavigationviewex/MainActivity.java
+++ BottomNavigationViewEx/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/bottomnavigationviewex/MainActivity.java
@@ -1,24 +1,55 @@
 package clc65.tuongng59.nguyenhuynhtuong.bottomnavigationviewex;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        bottomNav = findViewById(R.id.bottom_nav);
+        // Load Fragment mặc định
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+        }
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment;
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.nav_search) {
+                    selectedFragment = new SearchFragment();
+                } else if (itemId == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                } else {
+                    return false;
+                }
+                loadFragment(selectedFragment);
+                return true;
+            }
         });
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit();
     }
 }
