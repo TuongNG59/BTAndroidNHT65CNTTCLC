7d203ca01eb1e8be1cba1d0fe3de657f0dd58f61
TuongNG59 <nhuynhtuong59@gmail.com>
2025-12-02 21:06:40 +0700

    khai báo các nút và thêm sự kiện lăng nghe cho các nút để chuyển trang

--- Project/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/project/MainActivity.java
+++ Project/app/src/main/java/clc65/tuongng59/nguyenhuynhtuong/project/MainActivity.java
@@ -1,6 +1,8 @@
 package clc65.tuongng59.nguyenhuynhtuong.project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnPlay, btnManage, btnScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnManage = findViewById(R.id.btnManage);
+        btnScore = findViewById(R.id.btnScore);
+
+//        Chuyển qua màn hình chơi
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Chuyển qua PlayActivity
+            }
+        });
+
+
+//        Chuyển qua màn hình quản lý câu hỏi
+        btnManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        
+//        Chuyển qua màn hình điểm
+        btnScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
+
+
 }
